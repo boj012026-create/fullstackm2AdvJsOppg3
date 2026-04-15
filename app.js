433eb8a94ce242de5f997f84cdb23aba00e3eeab
Jan Olav Berg <boj012026@gmail.com>
2026-04-15 12:41:03 +0200

    Gets monsters, doesn't get images

--- app.js
+++ app.js
@@ -1,8 +1,52 @@
-console.log("hello world");
+const monsterContainer = document.getElementById("monster-container");
 
 //Todo Connect API
 const dndApi = {
-	baseUrl:  "https://www.dnd5eapi.co/api/2014/",
-	classes: `${this.baseUrl}classes`,
-	features: `${this.baseUrl}features`
+	url: "https://www.dnd5eapi.co/",
+	api2014: function() {return `${this.url}api/2014/`},
+	classes: function() {return `${this.api2014()}classes`},
+	features: function() {return `${this.api2014()}features`},
+	monsters: function() {return `${this.api2014()}monsters`},
+	spells: function() {return `${this.api2014()}spells`},
 }
+
+//prints a variable with function name. Debugging
+function print(funcName, objName, obj) {
+	console.log(`${funcName}()/${objName}:\n`, obj);
+}
+
+async function getJson(apiUrl) {
+	const response = await fetch(apiUrl);
+	print("getJson", "response", response) 
+	
+	if (response.ok) {
+		const json = await response.json()
+		print("getJson", "json", json);
+
+		return json;
+	}
+}
+
+async function monsterFactory(monsterJson) {
+	print("monsterFactory", "monsterJson", monsterJson);
+	const newMonsters = [];
+	monsterJson.results.forEach(monster => {
+		const monsterImg = document.createElement('img');
+		monsterImg.src = monster.image;
+		newMonsters.push(monsterImg);
+ 	});
+	return newMonsters;
+}
+
+function buildMonsters() {
+	getJson(dndApi.monsters())
+		.then(json => monsterFactory(json))
+		.then(monsters => monsterContainer.append(monsters));
+
+}
+
+function renderPage() {
+	buildMonsters();
+}
+
+renderPage();
