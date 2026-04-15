c72164fac545c9d2a6c46edf0e76542c52a9b25a
Jan Olav Berg <boj012026@gmail.com>
2026-04-15 09:29:14 +0200

    started a dndApi object for Urls

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log("hello world");
+
+//Todo Connect API
+const dndApi = {
+	baseUrl:  "https://www.dnd5eapi.co/api/2014/",
+	classes: `${this.baseUrl}classes`,
+	features: `${this.baseUrl}features`
+}
